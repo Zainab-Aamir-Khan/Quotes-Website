30335a9f208a7b29b1b095490e70f12e3990aece
Zainab Aamir <zaamir427@gmail.com>
2025-01-28 22:27:06 +0500

    second commit

--- main.py
+++ main.py
@@ -1,4 +1,15 @@
 from bs4 import BeautifulSoup
 import requests
 
-url = requests.get()
+url = requests.get('https://quotes.toscrape.com/').text
+soup = BeautifulSoup(url, 'lxml')
+
+content = soup.find('div', class_ = 'container')
+
+#extracting the main heading
+heading = content.find('div', class_ = 'col-md-8')
+print(heading.text)
+
+#extracting the quotes of a webpage
+quotes = content.find()
+
